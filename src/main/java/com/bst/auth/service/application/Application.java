46a65ea51b01a0e802a2b417208eb5667c448d37
ashwin <ashwinkn@hotmail.com>
2018-11-22 17:29:10 -0600

    Fixed component scan

--- src/main/java/com/bst/auth/service/application/Application.java
+++ src/main/java/com/bst/auth/service/application/Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.bst.auth.service.configuration.WebSecurityConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@ComponentScan(basePackageClasses = { WebSecurityConfiguration.class })
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
