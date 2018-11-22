5a03aee793ed8ed76d1763b30801568b8d2bd65b
ashwin <ashwinkn@hotmail.com>
2018-11-22 17:03:07 -0600

    Added permissive security configuration

---
+++ src/main/java/com/bst/auth/service/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.bst.auth.service.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.bst.user.authentication.components.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserService userService;
+
+	public WebSecurityConfiguration() {
+		super(false);
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authProvider() {
+		final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(this.userService);
+		authProvider.setPasswordEncoder(this.passwordEncoder);
+		return authProvider;
+	}
+
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(this.authProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry matchers = http
+				.antMatcher("/**").authorizeRequests();
+		matchers = matchers.antMatchers("/**/*").permitAll();
+		http.csrf().disable();
+	}
+
+	@Override
+	public void configure(final WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return this.authenticationManager();
+	}
+}
